6d20989a834ac7f5bf3e1cbe0ff1332e85509310
offorma <offorma@gmail.com>
2019-06-14 20:09:48 +0100

    commit before i revert

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+<<<<<<< HEAD
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 
     has_many :posts
     has_many :comments
+=======
+>>>>>>> parent of 4069bd6... Merge branch 'dev'
 end
