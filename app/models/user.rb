3b789da782eff5868b851ccccc8d800aedf1e717
offorma <offorma@gmail.com>
2019-06-14 20:10:37 +0100

    commit before i revert merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
 <<<<<<< HEAD
+<<<<<<< HEAD
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 
     has_many :posts
     has_many :comments
+=======
+>>>>>>> parent of 4069bd6... Merge branch 'dev'
 =======
 >>>>>>> parent of 4069bd6... Merge branch 'dev'
 end
