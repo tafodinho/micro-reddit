7ff3f7fa470f198233e64a3da50d24b09573124a
offorma <offorma@gmail.com>
2019-06-14 20:57:53 +0100

    just comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-
+    #one to many relationship
     has_many :posts
     has_many :comments
 end
